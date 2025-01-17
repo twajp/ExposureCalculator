52795ea18cec5ebecd1959d024e5c09f4ce7e3e7
TWA <64235761+twajp@users.noreply.github.com>
2025-01-18 07:00:52 +0900

    Rename widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:exposure_calculator/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ExposureCalculatorApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
