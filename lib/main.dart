9e3e657cd0e98c630f8e26f633351afa9f013da0
TWA <64235761+twajp@users.noreply.github.com>
2025-01-18 08:51:32 +0900

    Add some values

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'dart:math' as math;
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const ExposureCalculatorApp());
@@ -28,24 +28,88 @@ class ExposureCalculatorScreen extends StatefulWidget {
 }
 
 class _ExposureCalculatorScreenState extends State<ExposureCalculatorScreen> {
-  // 絞り値、シャッター速度、ISO の選択肢
-  final List<double> _apertureList = [4.0, 4.5, 5.0, 5.6, 6.3, 7.1, 8.0, 9.0, 10.0, 11.0, 13.0, 14.0, 16.0];
+  // https://www.scantips.com/lights/fstop2.html
+
+  // 絞り値の選択肢
+  final List<double> _apertureList = [1, 1.1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.5, 2.8, 3.2, 3.5, 4, 4.5, 5, 5.6, 6.3, 7.1, 8, 9, 10, 11, 13, 14, 16];
 
   // シャッター速度は「1/125」などを String で持ちたいので Map で管理
   final Map<String, double> _shutterMap = {
-    '1/60': 1.0 / 60.0,
-    '1/80': 1.0 / 80.0,
-    '1/100': 1.0 / 100.0,
-    '1/125': 1.0 / 125.0,
-    '1/160': 1.0 / 160.0,
-    '1/200': 1.0 / 200.0,
-    '1/250': 1.0 / 250.0,
+    '1/32000': 1 / 32768, // Full
+    '1/25600': 1 / 26008, // 1/3
+    '1/20000': 1 / 20643, // 2/3
+    '1/16000': 1 / 16384, // Full
+    '1/12800': 1 / 13004, // 1/3
+    '1/10000': 1 / 10321, // 2/3
+    '1/8000': 1 / 8192, // Full
+    '1/6400': 1 / 6502, // 1/3
+    '1/5000': 1 / 5161, // 2/3
+    '1/4000': 1 / 4096, // Full
+    '1/3200': 1 / 3251, // 1/3
+    '1/2500': 1 / 2580, // 2/3
+    '1/2000': 1 / 2048, // Full
+    '1/1600': 1 / 1625, // 1/3
+    '1/1250': 1 / 1290, // 2/3
+    '1/1000': 1 / 1024, // Full
+    '1/800': 1 / 812.7, // 1/3
+    '1/640': 1 / 645.1, // 2/3
+    '1/500': 1 / 512, // Full
+    '1/400': 1 / 406.4, // 1/3
+    '1/320': 1 / 322.5, // 2/3
+    '1/250': 1 / 256, // Full
+    '1/200': 1 / 203.2, // 1/3
+    '1/160': 1 / 161.3, // 2/3
+    '1/125': 1 / 128, // Full
+    '1/100': 1 / 101.6, // 1/3
+    '1/80': 1 / 80.63, // 2/3
+    '1/60': 1 / 64, // Full
+    '1/50': 1 / 50.8, // 1/3
+    '1/40': 1 / 40.32, // 2/3
+    '1/30': 1 / 32, // Full
+    '1/25': 1 / 25.4, // 1/3
+    '1/20': 1 / 20.16, // 2/3
+    '1/15': 1 / 16, // Full
+    '1/13': 1 / 12.7, // 1/3
+    '1/10': 1 / 10.08, // 2/3
+    '1/8': 1 / 8, // Full
+    '1/6': 1 / 6.35, // 1/3
+    '1/5': 1 / 5.04, // 2/3
+    '1/4': 1 / 4, // Full
+    '1/3': 1 / 3.175, // 1/3
+    '1/2.5': 1 / 2.52, // 2/3
+    '1/2': 1 / 2, // Full
+    '1/1.6': 1 / 1.587, // 1/3
+    '1/1.3': 1 / 1.26, // 2/3
+    '1': 1, // Full
+    '1.3': 1.26, // 1/3
+    '1.6': 1.52, // 2/3
+    '2': 2, // Full
+    '2.5': 2.52, // 1/3
+    '3': 3.175, // 2/3
+    '4': 4, // Full
+    '5': 5.04, // 1/3
+    '6': 6.35, // 2/3
+    '8': 8, // Full
+    '10': 10.08, // 1/3
+    '13': 12.7, // 2/3
+    '15': 16, // Full
+    '20': 20.16, // 1/3
+    '25': 25.4, // 2/3
+    '30': 32, // Full
   };
 
+  // ISO 感度の選択肢
   final List<int> _isoList = [50, 100, 200, 400, 800, 1600];
 
   // ND フィルターとそれが何ストップに相当するかを Map で表現
   final Map<String, double> _ndFilterMap = {
+    'No ND': 0,
+    'ND2 (1 stop)': 1,
+    'ND4 (2 stops)': 2,
+    'ND8 (3 stops)': 3,
+    'ND16 (4 stops)': 4,
+    'ND32 (5 stops)': 5,
+    'ND64 (6 stops)': 6,
     'ND128 (7 stops)': 7,
     'ND256 (8 stops)': 8,
     'ND400 (~8.6 stops)': 8.6,
@@ -61,7 +125,7 @@ class _ExposureCalculatorScreenState extends State<ExposureCalculatorScreen> {
   // New Exposure の初期値
   double _newAperture = 11.0;
   int _newISO = 100;
-  String _selectedNdFilterKey = 'ND512 (9 stops)';
+  String _selectedNdFilterKey = 'No ND';
 
   // 計算結果
   double _calculatedShutterSeconds = 0.0;
